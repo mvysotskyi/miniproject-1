906fffd990c944213ce8798f265618cf918737d5
mvysotskyi <vysotskyi.pn@ucu.edu.ua>
2023-04-06 20:52:41 +0300

    small changes

--- app/pandas_access/ingredient_show.py
+++ app/pandas_access/ingredient_show.py
@@ -19,12 +19,9 @@ def request_names(request, ingr_map):
         A dictionary with the matched ingredients.
     """
     a = ingr_map.copy()
-    a['replaced'].replace('flmy','flour',inplace=True)
+    a['replaced'].replace('flmy', 'flour', inplace=True)
     all_items = ','.join(list(set(a['replaced'])))
-    a.set_index('raw_ingr',inplace=True)
-    all_items = ',' +all_items
-    all_items = all_items + ','
+    a.set_index('raw_ingr', inplace=True)
+    all_items = ',' + all_items + ','
     results = re.findall(f',[ \w]*{request}[A-Za-z]*[ \w]*,', all_items)
-    results = [elem[1:-1] for elem in results]
-    results.sort(key = lambda x: len(x))
-    return {i: results[i] for i in range(min(8,len(results)))}
+    return sorted([elem[1:-1] for elem in results], key=len)[0:10]
